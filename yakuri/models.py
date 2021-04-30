adb7149ee9f62d55b0653f762952447f086aa691
Your Name <you@example.com>
2021-04-30 09:35:50 +0000

    init

--- yakuri/models.py
+++ yakuri/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+#講義名
+class Subjects(models.Model):
+    subjects=models.Charfield(verbose_name='講義名',max_length=10)
+    
+    def __str__(self):
+        return self.subjects
+        
+
+class　Fields(models.Model):
+    subject=models.ForiginKey('Subjects',on_delete=models.CASCADE)
+    
+    
 # Create your models here.
