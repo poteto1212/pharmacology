e6daf23d4c6e7f1bc807da426213c9c719a6f8c7
Your Name <you@example.com>
2021-05-27 02:41:22 +0000

    init

--- yakuri/context.py
+++ yakuri/context.py
@@ -7,6 +7,7 @@ yakurione="薬理学Ⅰ"
 yakuritwo="薬理学Ⅱ"
 yakurithree="薬理学Ⅲ"
 biseibutu="微生物学Ⅱ"
+bunsiseibutu="薬治Ⅳ(抗がん剤)"
 #全てのテンプレートに渡すデータを作成
 def related(request):
     context={
@@ -14,7 +15,8 @@ def related(request):
         'yakurione':Subject.objects.filter(subjects=yakurione).first(),
         'yakuritwo':Subject.objects.filter(subjects=yakuritwo).first(),
         'yakurithree':Subject.objects.filter(subjects=yakurithree).first(),
-        'biseibutu':Subject.objects.filter(subjects=biseibutu).first()
+        'biseibutu':Subject.objects.filter(subjects=biseibutu).first(),
+        'bunsiseibutu':Subject.objects.filter(subjects=bunsiseibutu).first()
     }
     
     return context
