e7a53081bbf1a4493644831ce5846769f2b6b5a6
Your Name <you@example.com>
2021-07-03 14:03:34 +0000

    init

--- pharmacology/settings.py
+++ pharmacology/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'admin_auto_filters',
+    #'admin_auto_filters',
     'dbbackup',
     'yakuri.apps.YakuriConfig',
     'linebot.apps.LinebotConfig',
