22273a043f78d327b7fd7ab386456867e97dcba8
Your Name <you@example.com>
2021-07-11 14:34:54 +0000

    init

--- linebotpharm/message/template_controller.py
+++ linebotpharm/message/template_controller.py
@@ -3,15 +3,15 @@ from django.db.models import Q
 def template_message(message):
 
     #薬品名の検索は弾く
-    if Detail.objects.filter(field__fields__icontains=message) and Detail.objects.filter(field__fields__icontains=message).count() < 10:
+    if Detail.objects.filter(field__fields__icontains=message):
         details=Detail.objects.filter(field__fields__icontains=message)
         return details
     
-    elif Detail.objects.filter(target__targets__icontains=message) and  Detail.objects.filter(target__targets__icontains=message).count()<10:
+    elif Detail.objects.filter(target__targets__icontains=message):
         details= Detail.objects.filter(target__targets__icontains=message)
         return details
 
-    elif Detail.objects.filter(detail__icontains=message) and Detail.objects.filter(detail__icontains=message).count()<10:
+    elif Detail.objects.filter(detail__icontains=message):
         details=Detail.objects.filter(detail__icontains=message)
         return details
     else:
